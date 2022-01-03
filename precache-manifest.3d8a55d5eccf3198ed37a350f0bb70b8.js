88e7c71050d0b8a29a1279b45d29c29876dd5989
Guilherme013S <guilhermemagno09@gmail.com>
2022-01-03 16:18:59 -0300

    Updates

---
+++ precache-manifest.3d8a55d5eccf3198ed37a350f0bb70b8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690b12e5c79d8f8f6e47d582a47046aa",
+    "url": "/google-keeper/index.html"
+  },
+  {
+    "revision": "480a69dc323d226f2327",
+    "url": "/google-keeper/static/js/2.4764cbf1.chunk.js"
+  },
+  {
+    "revision": "75db90c4bec1b1b0b5ae",
+    "url": "/google-keeper/static/js/main.321b5114.chunk.js"
+  },
+  {
+    "revision": "3aec56715cca8717456a",
+    "url": "/google-keeper/static/js/runtime-main.78fedc4d.js"
+  }
+]);
